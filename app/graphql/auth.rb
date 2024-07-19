d977b157181bccd459a16d88bb6581000469c473
jayne <jayne@jaynedebijixingdiannao.local>
2024-07-19 15:20:04 +0800

    rubocop

--- app/graphql/auth.rb
+++ app/graphql/auth.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Auth
   def current_user
     context[:current_user]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,41 +1,44 @@
+# frozen_string_literal: true
+
 # spec/models/user_spec.rb
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validations' do
     it 'requires a name' do
-      user = User.new(email: 'test@example.com', password: 'password', password_confirmation: 'password')
+      user = described_class.new(email: 'test@example.com', password: 'password', password_confirmation: 'password')
       expect(user).not_to be_valid
       expect(user.errors[:first_name]).to include("can't be blank")
     end
 
     it 'requires an email' do
-      user = User.new(first_name: 'Test', last_name: 'User', password: 'password', password_confirmation: 'password')
+      user = described_class.new(first_name: 'Test', last_name: 'User', password: 'password',
+                                 password_confirmation: 'password')
       expect(user).not_to be_valid
       expect(user.errors[:email]).to include("can't be blank")
     end
 
     it 'requires a unique email' do
-      existing_user = User.create(first_name: 'Existing', last_name: 'User', email: 'test@example.com', password: 'password', password_confirmation: 'password')
-      new_user = User.new(first_name: 'New', last_name: 'User', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+      described_class.create(first_name: 'Existing', last_name: 'User', email: 'test@example.com',
+                             password: 'password', password_confirmation: 'password')
+      new_user = described_class.new(first_name: 'New', last_name: 'User', email: 'test@example.com',
+                                     password: 'password', password_confirmation: 'password')
       expect(new_user).not_to be_valid
       expect(new_user.errors[:email]).to include('has already been taken')
     end
 
     it 'requires password confirmation' do
-      user = User.new(first_name: 'Test', last_name: 'User', email: 'test@example.com', password: 'password', password_confirmation: '')
+      user = described_class.new(first_name: 'Test', last_name: 'User', email: 'test@example.com', password: 'password',
+                                 password_confirmation: '')
       expect(user).not_to be_valid
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
   end
 
-  context 'associations' do
-    # todo: add associations
-  end
-
   context 'methods' do
     it 'returns the user\'s name' do
-      user = User.new(first_name: 'Test', last_name: 'User', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+      user = described_class.new(first_name: 'Test', last_name: 'User', email: 'test@example.com', password: 'password',
+                                 password_confirmation: 'password')
       expect(user.name).to eq('Test User')
     end
   end
