bbb4c790bfd0e2e588549f235c55c545265400e5
jayne <jayne@jaynedebijixingdiannao.local>
2024-07-20 15:06:52 +0800

    add userTable.roleId

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Role < ApplicationRecord
+  has_many :users, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
   validates :level, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
