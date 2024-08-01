3c4975a6fd605f851c89d03e4047aa72bf037f25
jayne <jayne@jaynedebijixingdiannao.local>
2024-08-01 10:15:49 +0800

    [Rails] add queryProducts API

---
+++ app/graphql/queries/products.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Queries
+  class Products < GraphQL::Schema::Resolver
+    description 'Get all products with pagination'
+
+    argument :page, Integer, required: false
+    argument :per_page, Integer, required: false
+
+    type [Types::ProductType], null: false
+
+    def resolve(page: 1, per_page: 20)
+      page = page.to_i
+      page = 1 if page < 1
+      per_page = per_page.to_i
+      per_page = 20 if per_page < 1
+
+      Product.includes(:categories, :creator, :updater)
+             .where(shop_id: context[:current_user].shops.pluck(:id))
+             .page(page)
+             .per(per_page)
+    end
+  end
+end
